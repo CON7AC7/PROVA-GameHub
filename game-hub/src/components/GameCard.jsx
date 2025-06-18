851bbf0e22503e644b77df4789a92f3de71c12ca
CON7AC7 <tutino.gian@gmail.com>
2025-06-18 15:24:26 +0200

    prova2

--- game-hub/src/components/GameCard.jsx
+++ game-hub/src/components/GameCard.jsx
@@ -1,18 +1,31 @@
-const GameCard = ({ game }) => {
-    return (
-      <div className="game-card">
-        <img src={game.coverUrl} alt={game.title} />
-        <h2>{game.title}</h2>
-        <p>{game.genre}</p>
-        <p>{game.platform}</p>
-        <p>{game.year}</p>
-        <p>{game.price}</p>
-        <p>{game.raiting}</p>
-        <p>{game.status}</p>
-        <p>{game.hoursPlayed}</p>
-        <p>{game.difficulty}</p>
-      </div>
-    );
-}
+import { useState } from "react";
 
-export default GameCard
+const GameCard = ({ game }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const toggleDetails = () => {
+    setShowDetails(prev => !prev);
+  };
+
+  return (
+    <div className="game-card" onClick={toggleDetails}>
+      <img src={game.coverUrl} alt={game.title} />
+      <h2>{game.title}</h2>
+      <p>{game.genre}</p>
+
+      {showDetails && (
+        <>
+          <p>{game.platform}</p>
+          <p>{game.year}</p>
+          <p>{game.price}</p>
+          <p>{game.rating}</p>
+          <p>{game.status}</p>
+          <p>{game.hoursPlayed}</p>
+          <p>{game.difficulty}</p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default GameCard;
