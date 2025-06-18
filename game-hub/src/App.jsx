f6ae9e0ffc672d3652abcbaf3fe34c316c275291
CON7AC7 <tutino.gian@gmail.com>
2025-06-18 16:19:00 +0200

    fix-navbar

--- game-hub/src/App.jsx
+++ game-hub/src/App.jsx
@@ -2,15 +2,25 @@ import "./App.css";
 import { getAllGames } from './data/mockData.js';
 import Header from  './components/header.jsx';
 import GameList from './components/GameList.jsx';
+import Navigation from './components/Navigation.jsx';
+import { useState } from 'react';
 
 function App() {
   const giochi = getAllGames();
   const gamerTag = "CON7AC7";
+  
+  const [activeTab, setActiveTab] = useState("Tutti");
+   const giochiFiltrati = activeTab === "Tutti"
+    ? giochi
+    : giochi.filter(gioco => gioco.status === activeTab);
+  
+ 
 
   return (
     <>
       <Header gamerTag={gamerTag} totalGames={giochi.length} />
-      <GameList games={giochi} />
+      <Navigation activeTab={activeTab} setActiveTab={setActiveTab} />
+      <GameList games={giochiFiltrati} />
     </>
   );
 }
---
+++ game-hub/src/components/Navigation.jsx
@@ -0,0 +1,29 @@
+import '../App.css';
+
+const tabs = [
+
+{label:"Tutti", value : "Tutti"},
+{label:"Completati", value : "Completati"},
+{label:"In Corso", value : "In Corso"},
+{label:"Wishlist", value : "Wishlist"},
+{label:"Abbandonati", value : "Abbandonati"},]
+
+const getTabClass = (isActive) => isActive ? "active-tab" : "idle-tab";
+
+const Navigation = ({ activeTab, setActiveTab }) => {
+  return (
+    <nav className="tab-navigation">
+      {tabs.map((tab) => (
+        <button
+          key={tab.value}
+          className={getTabClass(activeTab === tab.value)}
+          onClick={() => setActiveTab(tab.value)}
+        >
+          {tab.label}
+        </button>
+      ))}
+    </nav>
+  );
+};
+
+export default Navigation;
